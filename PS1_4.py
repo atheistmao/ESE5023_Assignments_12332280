34d40a4f0de97b79fb26c0658d203480fa38840d
atheistmao <1785803408@qq.com>
2023-10-25 11:28:20 +0800

    assignment

--- PS1_4.py
+++ PS1_4.py
@@ -3,14 +3,13 @@ import random
 
 def Least_moves(x):
   moves = 0
-  money = 1
 
-  while money < x:
-    if money*2 <= x: 
-      money = money * 2
+  while x > 1:
+    if x%2 == 0: 
+      x = x / 2
       moves += 1
     else:
-      money += 1
+      x = x - 1
       moves += 1
 
   print(moves)
